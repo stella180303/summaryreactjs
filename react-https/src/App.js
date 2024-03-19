de5a1855813ba550f69bff33c40dc1bbc49c68b8
stella180303 <stelladdvnx1803@gmail.com>
2024-03-19 12:05:46 +0700

    menambahkan folder implementasi react-https

---
+++ react-https/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import PostList from './Components/PostList';
+import PostForm from './Components/PostForm';
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm />
+    </div>
+  );
+}
+
+// <PostList />
+
+export default App;
